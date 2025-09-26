29daa6f6c151d8377e8877949f8f9be40da8532d
bowale-os <danbowale@gmail.com>
2025-09-25 19:18:55 -0500

    moved public into frontend

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  breakPreference: {type:String},
+  
 });
 
 module.exports = mongoose.model('User', userSchema);
